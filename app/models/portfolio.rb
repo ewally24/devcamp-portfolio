7d2e14fbc2aeb67efeb7e6c7a0d5002b7f2247e8
Errol Wallace <errolwallace@Errols-MBP.fios-router.home>
2017-12-03 22:06:55 -0500

    integrated default image defaults for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,4 +6,11 @@ class Portfolio < ApplicationRecord
     # Another Way to define scope.
     # scope :ruby_on_rails_portfolio_items, -> { where(subtitle: ‘angular’)}
   end
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= 'http://via.placeholder.com/600x400'
+    self.thumb_image ||= 'http://via.placeholder.com/350x200'
+  end
 end
