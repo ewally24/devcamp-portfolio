3fcd94b58f1e4f660da1c2cd512002452ea36b93
Errol Wallace <errolwallace@Errols-MBP.fios-router.home>
2017-12-05 23:50:17 -0500

    Integrated Nested Attributes

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,5 +1,7 @@
 class Portfolio < ApplicationRecord
   has_many :technologies
+  accepts_nested_attributes_for :technologies, reject_if: lambda {|attrs| attrs['name'].blank?}
+
   include Placeholder
 
   validates_presence_of :title, :subtitle, :body, :main_image, :thumb_image
@@ -10,6 +12,7 @@ class Portfolio < ApplicationRecord
     # scope :ruby_on_rails_portfolio_items, -> { where(subtitle: ‘angular’)}
   end
 
+  #runs after the instance of portfolio is created in the create def
   after_initialize :set_defaults
 
   def set_defaults
