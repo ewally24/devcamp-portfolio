0b86234550a2b2d55adbc7f3ce46f81c2f9cbafe
Errol Wallace <errolwallace@Errols-MacBook-Pro.local>
2017-12-10 14:00:54 -0500

    Completed Authentication with virtual attributes for user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  # before filter will run before any controllers because it's the Master Controller
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  # All of our DeviseWhitelist code was moved our new Devise Module
+  include DeviseWhitelist
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-  end
 end
